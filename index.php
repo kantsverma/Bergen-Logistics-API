cbb89e27575741e26a5a922ad1df3cec27b6a1fc
Kantsverma <dev.vini85@gmail.com>
2015-02-23 12:09:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
 * Author : devender Kumar
 * Twitter : @kantsverma
 ******************************************/ 
+
 ?>
 <html>
 <head>
-<title>Php Soap Test</title>
+<title>Bergen Logistics</title>
 <style type="text/css">
 body, html {font-family: Helvetica, Verdana, Arial; margin:10px; padding:0px; font-size:12px;}
 div.xml {height:200px;width:100%;overflow:auto;border:solid 1px #999;}
@@ -16,9 +17,13 @@ div.xml {height:200px;width:100%;overflow:auto;border:solid 1px #999;}
 </head>
 <body>
 <?php
-ini_set("soap.wsdl_cache_enabled", "0");
+  
+  
+  // Same as error_reporting(E_ALL);
+  ini_set("error_reporting", E_ALL);
+  ini_set("soap.wsdl_cache_enabled", "0");
 
-	try {        
+	try {
      $client = new SoapClient('http://sync.rex11.com/ws/v2prod/publicapiws.asmx?WSDL');
     //$client = new SoapClient('http://www.webservicex.net/country.asmx?WSDL');   
 	  $functions = $client->__getFunctions(); 
@@ -26,25 +31,93 @@ ini_set("soap.wsdl_cache_enabled", "0");
     // uncomment to check all function inside wsdl
     /*echo '<pre>';
     print_r($functions);
-    echo '<pre>'; */
+    echo '<pre>';*/
     
-    $params = array('WebAddress'=> 'www.drishnorthamerica.com','UserName'=> 'DRISH','Password'=> 'Bergen1');
+    $oathParams = array('WebAddress'=> 'www.drishnorthamerica.com','UserName'=> 'DRISH','Password'=> 'Bergen1');
     
-  	$result = $client->AuthenticationTokenGet($params);
+  	$result = $client->AuthenticationTokenGet($oathParams);
     
-	  echo $result->AuthenticationTokenGetResult;
-    //var_dump ($functions); 
-    exit;
-	
-    /*$params = array('CountryName'=> 'Australia');
-    $result = $client->GetISOCountryCodeByCountyName($params);
-    echo $result->GetISOCountryCodeByCountyNameResult;*/
+	  $auothToken =  $result->AuthenticationTokenGetResult;
     
-	}catch(SoapFault $fault) {		
-	  echo $fault->getMessage();
-	}
-?>
-  
+    // check if auoth token is generation then we will go ahead 
+    if($auothToken){
+      
+      // Pick ticket to 
+      
+      $auothTokenParams = array('AuthenticationString'=> $auothToken,
+                                
+                                'PickTicket'=> array(
+                                  'PickTicketNumber'=> '23022012012557',
+                                  'WareHouse'=> 'BERGEN LOGISTICS NJ',
+                                  'PaymentTerms'=> 'NET', 
+                                  'ShipVia'=> 'UPS',
+                                  'BillingOption'=> 'PREPAID',
+                                  'ShipService'=> 'UPS GROUND - COMMERCIAL',
+                                  'AuthorizationNumber'=> '50130A',
+                                  'BillToAddress'=> array(
+                                    'FirstName'=> 'Titus',
+                                    'LastName'=> 'Boss',
+                                    'CompanyName'=> 'Bergen shippers',
+                                    'Address1'=> '7300 Westside Ave',
+                                    'City'=> 'North Bergen',
+                                    'State'=> 'New Jersey', 
+                                    'Zip'=> '07047',
+                                    'Country'=> 'United States',
+                                    //'Non_US_Region'=> '',
+                                    'Phone'=> '(201) 854-1512',
+                                    //'Fax'=> '', 
+                                    'Email'=> 'test@test.com',
+                                  ),                                
+                                  'UseAccountUPS'=> '1',
+                                  'ShipToAddress'=> array(
+                                    'FirstName'=> 'Sergius',
+                                    'LastName'=> 'McU',
+                                    'CompanyName'=> 'NTSN',
+                                    'Address1'=> 'addr 1',
+                                    'City'=> 'Washington',
+                                    'State'=> 'Washington', 
+                                    'Zip'=> '20500',
+                                    'Country'=> 'USA',
+                                  ),
 
+                                  'LineItem'=> array(
+                                    'Season'=> 'v1',
+                                    'Style'=> '34113101',
+                                    'Description'=> 'DUNN 60MM WEDGE SLIDE- VI',
+                                    'Color'=> '236',
+                                    'Size'=> 'm',
+                                    'UPC'=> '888000000000',
+                                    'UnitPrice'=> 0.1, 
+                                    'Quantity'=> 1,
+                                    'Comments'=> 'test',
+                                  )
+                                ),
+                                
+                              );
+      
+      $pickTicket =  $client->PickTicketAdd($auothTokenParams);
+      
+      
+      // print the Authentication Token
+       echo '<div style="border-collapse:collapse;background-color:Wheat;color:black;"><b>Authentication Token Generated : </b> '.$auothToken.'</div><br/>';
+      
+      echo '<div style="border-collapse:collapse;background-color:Wheat;color:black;"><b>API Response PickTicketAdd: </b> </div><br/>';      
+      
+      // print response of pickticket 
+      echo '<pre>';
+      print_r($pickTicket);
+      echo '</pre>';
+
+    }
+	}catch(SoapFault $fault) {
+    
+	  echo $fault->getMessage();
+    
+	}
+
+?>
+<br/> 
+<br/>   
+<div>For more detail check here : <a href="http://sync.rex11.com/ws/v2prod/publicapiws.asmx" target="_blank">http://sync.rex11.com/ws/v2prod/publicapiws.asmx</a></div>
 </body>
 </html>
